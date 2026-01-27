d5045ab0271e07b2ab3d01002bdd3b1203034f3a
Dmitry Arkhipov <grisumbras@yandex.ru>
2026-01-27 09:51:19 +0300

    filter-out coverage data for irrelevant files

--- scripts/lcov-jenkins-experimental.sh
+++ scripts/lcov-jenkins-experimental.sh
@@ -84,7 +84,7 @@ collect_coverage () {
 
     cd "$BOOST_CI_SRC_FOLDER"
 
-    lcov --ignore-errors unused --remove coverage.info -o coverage_filtered.info '*/test/*' '*/extra/*'
+    lcov --ignore-errors unused --extract coverage.info "*/boost/$SELF/*" "*/$SELF/src/*" -o coverage_filtered.info
 }
 
 collect_coverage

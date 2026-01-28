a671c11dca72e837df4d0c804cd29144a39ecb4c
sdarwin <samuel.d.darwin@gmail.com>
2026-01-27 19:52:14 -0700

    gcovr 8.3 with templates

--- scripts/lcov-jenkins-gcc-13.sh
+++ scripts/lcov-jenkins-gcc-13.sh
@@ -38,7 +38,8 @@ if [ -f ${pythonvirtenvpath}/bin/activate ]; then
     source ${pythonvirtenvpath}/bin/activate
 fi
 
-pip3 install --upgrade gcovr || true
+# pip3 install --upgrade gcovr || true
+
 gcovr --version
 
 export B2_TOOLSET="gcc-13"
@@ -116,8 +117,7 @@ else
 fi
 
 outputlocation="$BOOST_CI_SRC_FOLDER/gcovr"
-# gcovr --merge-mode-functions separate -p --html-nested --html-template-dir=ci-automation/gcovr-templates/html --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
-gcovr --merge-mode-functions separate -p --html-nested --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
+gcovr --merge-mode-functions separate -p --html-nested --html-template-dir=ci-automation/gcovr-templates/html --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
 ls -al "${outputlocation}"
 
 # Generate tree.json for sidebar navigation

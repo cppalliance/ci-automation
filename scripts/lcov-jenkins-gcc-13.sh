bc884567e9957a07a940f5af8d56cc08ef8e2156
sdarwin <samuel.d.darwin@gmail.com>
2026-01-28 11:12:11 -0700

    gcovr back to 8.6 with --merge-lines

--- scripts/lcov-jenkins-gcc-13.sh
+++ scripts/lcov-jenkins-gcc-13.sh
@@ -38,7 +38,7 @@ if [ -f ${pythonvirtenvpath}/bin/activate ]; then
     source ${pythonvirtenvpath}/bin/activate
 fi
 
-# pip install --upgrade gcovr==8.4 || true
+pip install --upgrade gcovr==8.6 || true
 
 gcovr --version
 
@@ -117,7 +117,7 @@ else
 fi
 
 outputlocation="$BOOST_CI_SRC_FOLDER/gcovr"
-gcovr --merge-mode-functions separate -p --html-nested --html-template-dir=ci-automation/gcovr-templates/html --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
+gcovr --merge-mode-functions separate -p --html-nested --html-template-dir=ci-automation/gcovr-templates/html --merge-lines --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
 # gcovr --merge-mode-functions separate -p --html-nested --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
 ls -al "${outputlocation}"
 

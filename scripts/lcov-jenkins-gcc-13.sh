a9faad0e89bfb6544c8472ae228c7768b5fdbe0c
sdarwin <samuel.d.darwin@gmail.com>
2026-02-05 10:32:34 -0700

    gcovr sort uncovered-percent

--- scripts/lcov-jenkins-gcc-13.sh
+++ scripts/lcov-jenkins-gcc-13.sh
@@ -159,8 +159,8 @@ else
 fi
 
 outputlocation="$BOOST_CI_SRC_FOLDER/gcovr"
-gcovr --merge-mode-functions separate -p --html-nested --html-template-dir=ci-automation/gcovr-templates/html --html-title "$REPONAME" --merge-lines --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
-# gcovr --merge-mode-functions separate -p --html-nested --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
+gcovr --merge-mode-functions separate --sort uncovered-percent --html-nested --html-template-dir=ci-automation/gcovr-templates/html --html-title "$REPONAME" --merge-lines --exclude-unreachable-branches --exclude-throw-branches --exclude '.*/test/.*' --exclude '.*/extra/.*' --exclude '.*/example/.*' --filter "$GCOVRFILTER" --html --output "${outputlocation}/index.html"
+
 ls -al "${outputlocation}"
 
 # Generate tree.json for sidebar navigation

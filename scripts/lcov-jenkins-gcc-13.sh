6bb5422ec2f9444ca73bf28ed9e612697bcafdc9
sdarwin <samuel.d.darwin@gmail.com>
2026-06-16 13:56:07 -0600

    Testing gcovr 8.6-main

--- scripts/lcov-jenkins-gcc-13.sh
+++ scripts/lcov-jenkins-gcc-13.sh
@@ -102,8 +102,8 @@ fi
 # pip install --upgrade gcovr==8.6 || true
 # pip install --upgrade git+https://github.com/Spacetown/gcovr.git@05cbbc6f769da3671a3f659ad99198bac4d62dee || true
 # pip install --upgrade git+https://github.com/gcovr/gcovr.git@8dc1762283bbc30044f12e998b78e7f762e0f849 || true # multiple months. working fine.
-# pip install --upgrade git+https://github.com/gcovr/gcovr.git@2c6adcb521e88149c9256002ff95de8853bd6573 || true # right after new templates merge. problem.
-pip install --upgrade git+https://github.com/gcovr/gcovr.git@30a3fa2f36de12dd3694f26adce14c168563693e || true # new template bug fixes. Testing.
+pip install --upgrade git+https://github.com/gcovr/gcovr.git@2c6adcb521e88149c9256002ff95de8853bd6573 || true # right after new templates merge. problem.
+# pip install --upgrade git+https://github.com/gcovr/gcovr.git@30a3fa2f36de12dd3694f26adce14c168563693e || true # new template bug fixes. Testing.
 
 gcovr --version
 
@@ -216,6 +216,7 @@ if [ ! "$skipgcovroption" = "yes" ]; then
     --html \
     --output "$outputlocation/index.html" \
     --json-summary-pretty \
+    --html-theme=boost.green \
     --json-summary "$outputlocation/summary.json"
 
     # --html-template-dir=ci-automation/gcovr-templates/html \
